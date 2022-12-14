4fd74d6651d5277508a8d033b75865ffa14e169e
berhanez <birhanez@gmail.com>
2022-12-14 11:24:23 +0300

    get_op function created

---
+++ get_op.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+/**
+ * get_op - gets the operation from token
+ * @tok: the operation
+ * Return: a function pointer
+ */
+void (*get_op(char *tok))(stack_t **stack, unsigned int line_number)
+{
+	instruction_t ops[] = {
+		{ "push", push },
+		{ "pall", pall },
+		{ "pint", pint },
+		{ "pop", pop },
+		{ NULL, NULL }
+	};
+	int i;
+
+	for (i = 0; ops[i].opcode != NULL; i++)
+	{
+		if (strcmp(ops[i].opcode, tok) == 0)
+			return (ops[i].f);
+	}
+}
