8d163b70f05c5955d7dbe97d0f24b2bbc4db0582
berhanez <birhanez@gmail.com>
2022-12-15 11:57:48 +0300

    procline ammended in header and free_stack proto added

--- monty.h
+++ monty.h
@@ -51,7 +51,9 @@ void pchar(stack_t **stack, unsigned int line_number);
 void pstr(stack_t **stack, unsigned int line_number);
 void rotl(stack_t **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
-void proc_line(char *buffer, unsigned int line_number, stack_t **stack);
+void proc_line(char *buffer, unsigned int line_number, stack_t **stack, FILE
+*monty_file);
+void free_stack(stack_t *stack);
 void rotr(stack_t **stack, unsigned int line_number);
 void stack_set(stack_t **stack, unsigned int line_number);
 void queue_set(stack_t **stack, unsigned int line_number);
